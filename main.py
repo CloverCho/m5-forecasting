add614cc6cda88108df9d1427cbb7a74bff811a6
CloverCho <43602849+CloverCho@users.noreply.github.com>
2021-05-21 12:47:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,28 +34,48 @@ def main():
     data_path = os.path.join(dir_path, './data')
 
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
 
     ctf_indexs, cthh_indexs, cthb_indexs, wif_indexs, wihh_indexs, wihb_indexs = make_cluster(data_path = data_path)
     
 
     stv_path = os.path.join(data_path, './sales_train_validation.csv')
+    ste_path = os.path.join(data_path, './sales_train_evaluation.csv')
+    sub_path = os.path.join(data_path, './sample_submission.csv')
+
     stv = pd.read_csv(stv_path).iloc[:, 6:]
-    
+    ste = pd.read_csv(ste_path).iloc[:, -28:]
+    submission = pd.read_csv(sub_path)
+
     ctf_index_1 = ctf_indexs[0]
 
     X = stv.loc[ctf_index_1].astype(np.int16).T
     print(X.head())
+
+    pred_X = ste.loc[ctf_index_1].astype(np.int16).T
+    print(pred_X.head())
 
     scaler = StandardScaler()
     scaler = scaler.fit(X)
     X = scaler.transform(X)
     print(X.shape)
 
+    pred_scaler = StandardScaler()
+    pred_scaler = pred_scaler.fit(pred_X)
+    pred_X = pred_scaler.transform(pred_X)
+    print(pred_X.shape)
+    print('hello')
+
+
     seq_length = 28
     x, y = slidng_windows(X, seq_length)
+    
+
+
     print(x.shape)
     print(y.shape)
+
 
 
     train_size = int(len(y) * 0.67)
@@ -70,6 +90,8 @@ def main():
     testX = torch.Tensor(np.array(x[train_size:len(x)]))
     testY = torch.Tensor(np.array(y[train_size:len(y)]))
 
+
+
     print("train shape is: ", trainX.size())
     print("train label shape is: ", trainY.size())
     print("test shape is: ", testX.size())
@@ -78,7 +100,7 @@ def main():
 
 
     ############# Parameters ###################
-    num_epochs = 500
+    num_epochs = 30
     learning_rate = 1e-3
     input_size = np.array(trainX.shape)[2]
     hidden_size = 512
@@ -114,18 +136,41 @@ def main():
         vall_loss = criterion(valid, testY.to(device))
         scheduler.step(vall_loss)
 
-        if epoch % 50 == 0:
+        if epoch % 10 == 0:
             print("Epoch: %d, loss: %1.5f valid loss: %1.5f " % (epoch, loss.cpu().item(),vall_loss.cpu().item()))
 
 
     ############# Prediction ###############
     lstm.eval()
-    train_predict = lstm(dataX.to(device))
-    data_predict = train_predict.cpu().data.numpy()
-    print(data_predict[:5])
-
     
 
+
+    pred_data = torch.Tensor(np.expand_dims(np.array(pred_X), axis=0))
+    pred_1914 = lstm(pred_data.to(device)).cpu().data.numpy()
+    
+    pred_y = np.copy(pred_1914)
+
+
+    for i in range(1, 28):
+        pred_data = np.array(pred_X[i:])
+        pred_data = np.concatenate((pred_data, pred_y), axis=0)
+        pred_data = torch.Tensor(np.expand_dims(pred_data, axis=0))
+        pred_result = lstm(pred_data.to(device)).cpu().data.numpy()
+        pred_y = np.concatenate((pred_y, pred_result), axis = 0)
+
+    print(pred_y.shape)
+    pred_y = pred_y.T       
+    print(pred_y[:5])
+    submission.loc[ctf_index_1].iloc[:,1:] = pred_y
+    submission.iloc[:, 1:] = submission.iloc[:, 1:].astype(np.int16)
+    submission.to_csv(r'./submission.csv', index=False)
+
+
+    #train_predict = lstm(dataX.to(device))
+    #train_predict = lstm(pred_X.to(device))
+    #data_predict = train_predict.cpu().data.numpy()
+    #print(data_predict[:5])
+    #print(data_predict.shape)
 
 
 
