9354c39b3519d6fec73c64cd10858e7c6cbe8a74
ming201702 <76940505+ming201702@users.noreply.github.com>
2021-05-21 12:17:13 +0900

    Update model.py

--- model/model.py
+++ model/model.py
@@ -34,5 +34,34 @@ class LSTM(nn.Module):
 
         return out
 
+ 
+class SingleRNN(nn.Module):
+
+    def __init__(self, num_classes, input_size, hidden_size, num_layers):
+        super(SingleRNN, self).__init__()
+
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(p = 0.2)
+        
+        self.rnn = nn.GRU(input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, dropout=0.25)
+        
+        self.fc = nn.Linear(hidden_size, num_classes)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    def forward(self, x):
+
+        h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        
+        ula, h_out = self.rnn(x, h_0)
+
+        h_out = h_out.view(-1, self.hidden_size)
+        out = self.fc(h_out)
+        out = self.dropout(out)
+
+        return out
 
 
