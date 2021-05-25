3d1d621c81bd1b126aff88ecf54ccd42ffdfc262
CloverCho <snsc7878@gmail.com>
2021-05-25 15:34:01 +0900

    update main.py and LStm.py

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ from model.model import LSTM
 from model.losses import RMELoss
 from train.LSTM import singleLSTM
 
+def slidng_windows(self, data, seq_length):
+    x = []
+    y = []
+
+    for i in range(len(data) - seq_length - 1):
+        _x = data[i:(i+seq_length)]
+        _y = data[i+seq_length]
+        x.append(_x)
+        y.append(_y)
+
+    return np.array(x), np.array(y)
+
 
 
 def main():
