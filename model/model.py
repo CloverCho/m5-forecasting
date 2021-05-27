c1773650c7f3ec7d8f4645c6bb1e8ec603652be1
CloverCho <snsc7878@gmail.com>
2021-05-28 02:10:11 +0900

    Update model.py

--- model/model.py
+++ model/model.py
@@ -208,3 +208,30 @@ class GRU(nn.Module):
         return out
     
 # class Encoder(nn.Module):
+
+
+
+class DilatedCNN(nn.Module):
+    # https://vaibhaw-vipul.medium.com/building-a-dilated-convnet-in-pytorch-f7c1496d9bf5
+    def __init__(self):
+        super(DilatedCNN, self).__init__()
+        self.convlayers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=9, stride=1, padding=0, dilation=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=0, dilation=2),
+            nn.ReLU(),
+        )
+
+        self.fclayers = nn.Sequential(
+            nn.Linear(2304, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        x = self.convlayers(x)
+        x = x.view(-1, 2304)
+        out = self.fclayers(x)
+        return out
