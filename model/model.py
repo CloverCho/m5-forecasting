e06a541cb314987f7bb18af4599d7da5757117b6
ming201702 <76940505+ming201702@users.noreply.github.com>
2021-05-28 12:17:36 +0900

    Update model.py

--- model/model.py
+++ model/model.py
@@ -76,19 +76,10 @@ class LSTM(nn.Module):
         self.batch_size = 1
         # self.seq_length = seq_length
 
-        self.LSTM = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True,
-                             dropout=0.2)
+        self.LSTM = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, dropout=0.2)
 
-        self.fc1 = nn.Linear(hidden_size, 256)
-        self.bn1 = nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-        self.dp1 = nn.Dropout(0.25)
-
-        self.fc2 = nn.Linear(256, 128)
-
-        self.bn2 = nn.BatchNorm1d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-        self.dp2 = nn.Dropout(0.2)
-        self.fc3 = nn.Linear(128, 1)
-        self.relu = nn.ReLU()
+        self.fc = nn.Linear(hidden_size, num_classes)
+        self.dp = nn.Dropout(p=0.2)
 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -100,27 +91,16 @@ class LSTM(nn.Module):
             self.num_layers, x.size(0), self.hidden_size).to(self.device))
 
         _, (hn, cn) = self.LSTM(x, (h_1, c_1))
-
         print("hidden state shape is:",hn.size())
-        y = hn.view(-1, self.hidden_size)
 
         final_state = hn.view(self.num_layers, x.size(0), self.hidden_size)[-1]
         print("final state shape is:",final_state.shape)
 
-        x0 = self.fc1(final_state)
-        x0 = self.bn1(x0)
-        x0 = self.dp1(x0)
-        x0 = self.relu(x0)
-
-        x0 = self.fc2(x0)
-        x0 = self.bn2(x0)
-        x0 = self.dp2(x0)
-
-        x0 = self.relu(x0)
-
-        out = self.fc3(x0)
-        print(out.size())
+        out = self.fc(final_state)
+        out = self.dp(out)
+        
         return out
+    
     
 
 class singleGRU(nn.Module):
